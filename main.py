0cb38648d3737c3fa2a9ee821b26f62114943c40
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-16 20:33:27 +0500

    #4 Сделал тестовый вариант бота

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-print('1')
+import telebot
+import datetime
+from settings import TOKEN
 
-print('hi')
+bot = telebot.TeleBot(TOKEN)
 
-print('33')
+@bot.message_handler(content_types=['text'])
+def get_text_message(message):
+    username = message.from_user.username
+    msg=''
+    f = open(f'log {username}.txt', 'a')
 
-print('2')
+    if message.text == 'Привет':
+        msg = f'Привет {username}! Хочешь анекдот?'
+        bot.send_message(message.from_user.id, msg)
+    elif message.text == 'Нет' or message.text == 'нет':
+        msg = f'Ну, давай. Хотя бы улыбнешься.\tСогласен?'
+        bot.send_message(message.from_user.id, msg)
+    elif message.text == 'Да' or message.text == 'да':
+        msg = f'Анекдота нет! Повёлся.'
+        bot.send_message(message.from_user.id, msg)
+    elif message.text == 'хочу' or message.text == 'Хочу':
+        msg = f'Нужно писать "Да" или "Нет"'
+        bot.send_message(message.from_user.id, msg)
+    else:
+        msg = f'Я тебя не понимаю =('
+        bot.send_message(message.from_user.id, msg)
+
+    # Записываем логи в файл с id пользователем
+    f.write(f'{datetime.datetime.now()}:\t{message.text}\n')
+    if msg!='':
+        f.write(f'{datetime.datetime.now()}:\t{msg}\n')
+    f.close()
+
+bot.polling()
