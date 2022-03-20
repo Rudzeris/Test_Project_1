0cb2c056fd5a12f76680ab26083056502959fddb
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-20 21:35:51 +0500

    #6 Начало пользования шаблонами

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 from django.shortcuts import render
 
 def hello(request):
-    response = HttpResponse('Hisame')
-    return response
+    t=Template('<h1>he he {{name}}</h1>')
+    c=Context({'name':'Rudzeris'})
+    return HttpResponse(t.render(context=c))
