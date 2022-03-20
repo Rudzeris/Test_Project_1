39cde9644655ff3f1db3ee47a5693ce387ad6ebf
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-20 21:57:50 +0500

    #6 Создал класс Book. Migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField('Название', max_length=128)
+    pages = models.IntegerField('Количество страниц', blank=True, null=True)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
-from django.template import Template, Context
 from django.shortcuts import render
 
 def hello(request):
-    t=Template('<h1>he he {{name}}</h1>')
-    c=Context({'name':'Rudzeris'})
-    return HttpResponse(t.render(context=c))
+    return render(request, 'core/hello.html', {'name':'Rudzeris'})
