64b2e877cb35cbc90f36f9f8d14c79df90a397da
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-21 23:10:02 +0500

    #7 Создал автора для книг

--- core/models.py
+++ core/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField('Имя', max_length=128)
+
+
 class Book(models.Model):
+    author = models.ForeignKey('core.Author', on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField('Название', max_length=128)
     pages = models.IntegerField('Количество страниц', blank=True, null=True)
 
