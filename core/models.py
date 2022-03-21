bde62f65eef4a718294f781ba514167a0e492a69
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-21 23:14:41 +0500

    #7 Добавил заказы в БД

--- core/models.py
+++ core/models.py
@@ -5,6 +5,10 @@ class Author(models.Model):
     name = models.CharField('Имя', max_length=128)
 
 
+class AuthorProfile(models.Model):
+    author = models.OneToOneField
+
+
 class Book(models.Model):
     author = models.ForeignKey('core.Author', on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField('Название', max_length=128)
@@ -12,3 +16,8 @@ class Book(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    date = models.DateField()
+    book = models.ManyToManyField('core.Book')
