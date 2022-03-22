22a275783437576c42d7d4e3ca025727f60958c4
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-22 21:35:04 +0500

    #8 Создал страницу списка книг и страницы для каждой книги

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ app_name = 'core'
 urlpatterns = [
     path('', core.views.index, name='index'),
     path('books/', core.views.book_list, name='book_list'),
-    path('books/<int:pk>/', core.views.book_list, name='book_list'),
+    path('books/<int:pk>/', core.views.book_detail, name='book_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 import core.models
 
@@ -5,11 +6,12 @@ def index(request):
     books= core.models.Book.objects.all()
     return render(request, 'core/index.html', {'books':books})
 
-def book_list(request, pk):
+def book_list(request):
     books= core.models.Book.objects.all()
-    return render(request, 'core/index.html', {'books':books})
+    return render(request, 'core/book_list.html', {'books':books})
 
 def book_detail(request, pk):
+    return HttpResponseNotFound('Не найдено')
     book= core.models.Book.objects.get(pk=pk)
-    return render(request, 'core/index.html', {'book':book})
+    return render(request, 'core/book_detail.html', {'book':book})
 
