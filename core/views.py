72b074024fbc7bb1408f324060d698d53dab73a3
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-22 22:47:50 +0500

    #8 Добавил классов и title. Конец.

--- core/views.py
+++ core/views.py
@@ -6,8 +6,29 @@ from django.views.generic import TemplateView, ListView, DetailView
 import core.models
 
 
-class IndexView(TemplateView):
+class TitleMixin:
+    title = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = self.get_title()
+        return context
+
+    def get_title(self):
+        return 'Главная страница'
+
+
+class IndexView(TitleMixin,TemplateView):
     template_name = 'core/index.html'
+    title = 'Главная страница'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['info'] = self.get_info()
+        return context
+
+    def get_info(self):
+        return 'Главная страница'
 
 
 class Books(ListView):
