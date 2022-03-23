2a3861eb9ae7832947b80957a03aadfbe43d6d3e
Rudzeris <ruzil1atangulov@gmail.com>
2022-03-23 17:57:56 +0500

    #10 Добавили функцию для проверки количества страниц.

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
+import core.models
+
 
 class BookSearch(forms.Form):
-    name = forms.CharField(label='Название', required=False, widget=forms.Textarea)
+    name = forms.CharField(label='Название', required=False)
     min_pages = forms.IntegerField(label='Кол-во страниц', required=False, help_text='Минимальное кол-во страниц')
+
+    def clean_min_pages(self):
+        cleaned_data = self.cleaned_data
+        min_pages = cleaned_data.get('min_pages')
+        if(min_pages and  min_pages>1000):
+            raise forms.ValidationError('Количество страниц не должно быть больше 1000')
+        return cleaned_data
+
+class BookEdit(forms.ModelForm):
+    class Meta:
+        model = core.models.Book
+        fields = '__all__'
+
+
